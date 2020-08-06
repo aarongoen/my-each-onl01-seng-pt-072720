ffef0d59be9338b75913d24f121b14dd536be6e5
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-06 14:24:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(words)
+if block_given? == true
+  counter = 0 
+  
+  while counter < words.length 
+    yield(words[counter])
+  counter += 1
+  end
+
+  words
+elsif block_given? == []
+  "This block should not run!"
+else block_given? == false
+  "Needs block to evaluate."
+end
 end
