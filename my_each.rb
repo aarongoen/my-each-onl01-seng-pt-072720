fc330c9b2fa4f4887de26aadb92a0164aeb27c65
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-06 14:17:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,15 +4,15 @@ def my_each(words)
 if block_given? == TRUE
   counter = 0 
   
-  while i < words.length 
+  while counter < words.length 
     yield(words[counter])
   counter += 1
   end
 
   words
 elsif block_given? == []
-  puts "This block should not run!"
+  "This block should not run!"
 else block_given? == FALSE
-  puts "Needs block to evaluate."
+  "Needs block to evaluate."
 end
 end
